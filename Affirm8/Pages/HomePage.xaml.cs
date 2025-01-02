dce874fb4b5c9c9e6606e53989c9d51b7f7d8dfc
QRY91 <hauntingqry@gmail.com>
2025-01-02 16:46:13 +0100

    refactor collection layout, add datadummy function

--- Affirm8/Pages/HomePage.xaml.cs
+++ Affirm8/Pages/HomePage.xaml.cs
@@ -3,8 +3,10 @@ namespace Affirm8.Pages;
 public partial class HomePage : ContentPage
 {
     public class Post {
-        public int PostId {get; set; }
+        public int PostId { get; set; }
         public string Content { get; set; }
+        public int UserId { get; set; }
+        public DateTime Timestamp { get; set; }
     }
     List<Post> posts = new List<Post>();
 
@@ -16,16 +18,22 @@ public partial class HomePage : ContentPage
 
     private void InitCollectionView()
     {
-        GenerateDummyPosts();
+        GenerateDummyPosts(15);
         CollViewPosts.ItemsSource = posts;
     }
 
-    private void GenerateDummyPosts()
+    private void GenerateDummyPosts(int repetitions)
     {
-        posts.Add(new Post() { PostId = 0, Content = "first!!1!" });
-        posts.Add(new Post() { PostId = 0, Content = "any1 still watching in 2025?" });
-        posts.Add(new Post() { PostId = 0, Content = "updog" });
-        posts.Add(new Post() { PostId = 1, Content = "want to get rich? click here: (www.scam.com)" });
+        var dummyCount = 4;
+        var postId = 0;
+        for (int i = 0; i < repetitions; i++)
+        {
+            postId = i * dummyCount;
+            posts.Add(new Post() { PostId = postId + 0, Content = "first!!1!", UserId = 0, Timestamp = DateTime.UtcNow });
+            posts.Add(new Post() { PostId = postId + 1, Content = "any1 still watching in 2025?", UserId = 0, Timestamp = DateTime.UtcNow });
+            posts.Add(new Post() { PostId = postId + 2, Content = "updog", UserId = 0, Timestamp = DateTime.UtcNow });
+            posts.Add(new Post() { PostId = postId + 3, Content = "want to get rich? click here: (www.scam.com)", UserId = 0, Timestamp = DateTime.UtcNow });
+        }
     }
 
     private async void BtnNavProfile_Clicked(object sender, EventArgs e)
