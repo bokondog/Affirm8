5e9f88ffd988f5a32028d341b610a0e20d032d83
QRY91 <hauntingqry@gmail.com>
2025-01-08 17:01:02 +0100

    add popup

--- Affirm8/Views/LoginPage.xaml.cs
+++ Affirm8/Views/LoginPage.xaml.cs
@@ -5,11 +5,19 @@ namespace Affirm8.Views.Forms
         public LoginPage()
         {
             InitializeComponent();
+            InitPopup();
+        }
+
+        private void InitPopup()
+        {
+            syncPopup.HeaderTitle = "Login success";
+            syncPopup.Message = "You have successfully logged in! Congratulations, user";
         }
 
         private void loginButton_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Login SUCCESS", "You have successfully logged in! Congratulations, user", "...ok");
+            // DisplayAlert("Login SUCCESS", "You have successfully logged in! Congratulations, user", "...ok");
+            syncPopup.Show();
         }
     }
 }
