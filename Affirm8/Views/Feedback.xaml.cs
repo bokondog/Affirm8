db6ba8ed7113c98af52490d943fe05a0e1019c07
QRY91 <hauntingqry@gmail.com>
2025-01-09 16:56:02 +0100

    add sf examples

---
+++ Affirm8/Views/Feedback.xaml.cs
@@ -0,0 +1,10 @@
+namespace Affirm8.Views.Feedback
+{
+    public partial class Feedback : ContentPage
+    {
+        public Feedback()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Affirm8/Views/Review.xaml.cs
@@ -0,0 +1,11 @@
+namespace Affirm8.Views.Feedback
+{
+    public partial class Review : ContentPage
+    {
+        public Review()
+        {
+            InitializeComponent();
+            this.ProductImage.Source = App.ImageServerPath + "Image1.png";
+        }
+    }
+}
