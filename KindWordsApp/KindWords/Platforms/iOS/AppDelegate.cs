3ccf01ee42c13ffe96871b239a70d74491220456
bokondog <glenn.bokondo@student.pxl.be>
2025-08-18 17:16:13 +0200

    remove old directories

--- KindWordsApp/KindWords/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace KindWords;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
--- api/ExampleWebApi/Services/Contracts/ITokenFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using ExampleWebApi.Domain;
-
-namespace ExampleWebApi.Api.Services.Contracts;
-//DO NOT TOUCH THIS FILE!!
-
-public interface ITokenFactory
-{
-    string CreateToken(User user, IList<string> roleNames);
-}
